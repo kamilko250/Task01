d061ada99bab376021267dd8c1d1533e128fb871
kamilko250 <freshmultikk21@gmail.com>
2020-04-08 21:53:12 +0200

    Revert "Update Program.cs"
    This reverts commit 5d517898afff672345a5d65dbe12b08ee9238bc9.

--- Program.cs
+++ Program.cs
@@ -18,10 +18,10 @@ namespace ReadFileXML
             while (reader.Read())
             {
 
-                if (reader.NodeType == XmlNodeType.Element && reader.Name == "number" )
+                if (reader.NodeType == XmlNodeType.Element && reader.Name == "number")
                 {
                     string s1 = reader.ReadElementString();
-                    if (scanOrNo % 2 == 0 && reader.GetAttribute("number") != "igonre-it")
+                    if (scanOrNo % 2 == 0)
                         sum += int.Parse(s1);
 
                     Console.WriteLine(s1);
